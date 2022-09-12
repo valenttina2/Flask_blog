6166f08a7f2b22574254ff1f75a9269a6f2e0625
Andrei <valuschika@gmail.com>
2022-09-12 14:52:39 +0300

    html, main.css

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+# import os
+# import sys
+# project_root = os.path.dirname(__file__)
+# template_path = os.path.join(project_root, '../templates')
+# static_path = os.path.join(project_root, '../static')
+# app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 app = Flask(__name__)
 
