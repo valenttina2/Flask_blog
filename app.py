74924e4d180d60ed7e37bb55d00f63fb8857360f
Andrei <valuschika@gmail.com>
2022-09-13 15:20:16 +0300

    sqlalchemy

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 # import os
 # import sys
 # project_root = os.path.dirname(__file__)
@@ -7,6 +10,21 @@ from flask import Flask, render_template, url_for
 # app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
+
+db=SQLAlchemy(app)
+
+class Article(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    category = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Article %r>' %self.id
+
 
 @app.route('/')
 def index():
